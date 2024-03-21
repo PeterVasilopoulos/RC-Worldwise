6aee23def7e15509fd1da3656f9d3ab942232e32
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-21 16:01:28 -0400

    added links between pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,15 @@
+import {Link} from 'react-router-dom'
+import PageNav from '../../components/PageNav'
+
 function Homepage() {
     return (
         <div>
-            WorldWise
+            <PageNav />
+            <h1>
+                WorldWise
+            </h1>
+
+            <Link to='/pricing'>Pricing</Link>
         </div>
     )
 }
