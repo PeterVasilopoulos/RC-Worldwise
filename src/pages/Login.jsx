ccdbee8e7a41bf3b301fa823cf65aaabb3ebc9b9
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-23 16:35:30 -0400

    removed parts of code that were ahead of course

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "../components/Button";
+import { useState } from "react";
 import PageNav from "../components/PageNav";
-import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./Login.module.css";
 
 export default function Login() {
@@ -10,27 +7,11 @@ export default function Login() {
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
-  const { login, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (email && password) login(email, password);
-  }
-
-  useEffect(
-    function () {
-      if (isAuthenticated) navigate("/app", { replace: true });
-    },
-    [isAuthenticated, navigate]
-  );
-
   return (
     <main className={styles.login}>
       <PageNav />
 
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -52,7 +33,6 @@ export default function Login() {
         </div>
 
         <div>
-          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
